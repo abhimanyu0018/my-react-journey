6e65aa9b746759c88a271828c50a31a626b37fef
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-01-21 14:39:48 +0530

    my first react project with vite framework

---
+++ firstReactWithVite/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+   <div className='main'>
+    <h1>Abhimanyu Kanaujia</h1>
+    <div>
+      <p>My first ever React project.</p>
+    </div>
+   </div>
+  )
+}
+
+export default App
