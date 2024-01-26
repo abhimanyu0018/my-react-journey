36be1ba9ed5cbf79ae30d273da6ddade9f3dcb39
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-01-26 23:04:02 +0530

    updated password generator

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,24 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(5);
+  const [isNum, setIsNum] = useState(false);
+  const [isChar, setIsChar] = useState(false);
 
   return (
     <>
       <div className="main">
         <div className="input">
           <input type="text" />
-          <button>copy</button>
+          <button className="copy">copy</button>
         </div>
 
         <div className="control">
-          <input type="range" min={5} max={10}/>
-          <label> length {}</label>
+          <input
+            type="range"
+            min={0}
+            max={10}
+            defaultValue={5}
+            onChange={(e) => {
+              setCount(e.target.value);
+            }}
+          />
+          <label> length {count}</label>
+
+          <input
+            type="checkbox"
+            onClick={() => {
+              setIsNum(!isNum);
+            }}
+          />
+          <label>Number</label>
+
+          <input
+            type="checkbox"
+            onClick={() => {
+              setIsChar(!isChar);
+            }}
+          />
+          <label>Special Characters</label>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
