e1b4d08747a2bb005eb6e673e033d4a3d92c1065
Abhimanyu Kanaujia <iabhimanyu.kanaujia@gmail.com>
2024-02-16 14:33:03 +0530

    edit in app.jsx

--- BasicRegistrationFormApp/src/App.jsx
+++ BasicRegistrationFormApp/src/App.jsx
@@ -11,13 +11,23 @@ function App() {
 
 
 
-  const submit = false
+  const [submit,setSubmit] = useState(false)
   const [isFirstName,setIsFirstName] = useState(false)
   const [isLastName,setIsLastName] = useState(false)
   const [isEmail,setIsEmail] = useState(false)
 
   const submitFunc = ()=>{
-    pre
+    preventDefault() 
+
+  }
+
+  const handler = ()=>{
+    if(!value.firstName || !value.lastName || !value.email){
+      setSubmit(false)
+    }
+    else{
+      setSubmit(true)
+    }
   }
 
   return (
@@ -32,20 +42,20 @@ function App() {
               setValue({...value,firstName: e.target.value})
               setIsFirstName(true)
              }} /> 
-             <span>this field is reqired</span> 
+             {submit && !value.firstName ? <span>this field is reqired</span>  : null }
 
              <input type="text" name="" id="" placeholder="Enter Last Name" onChange={(e)=>{
               setValue({...value,lastName: e.target.value})
               setIsLastName(true)
              }} />
-             <span>this field is reqired</span> 
+             {submit && !value.lastName ? <span>this field is reqired</span>  : null }
 
              <input type="text" placeholder='Email..' onChange={(e)=>{
               setValue({...value,email: e.target.value})
              }} />
-             <span>this field is reqired</span> 
+             {submit && !value.email ? <span>this field is reqired</span>  : null }
 
-             <button type="submit">Submit</button>
+             <button type="submit" onClick={handler}>Submit</button>
 
           </form>
         </div>
